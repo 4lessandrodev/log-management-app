06e4214e1d0d7940f234c84785b7531e654f10b7
4lessandrodev <alessandroadm@live.com>
2023-02-02 03:43:50 -0300

    feat: getting error from catch

--- use-case/payment.use-case.ts
+++ use-case/payment.use-case.ts
@@ -21,6 +21,19 @@ export class Payment implements IUseCase<Dto, Result<void, Steps>>{
             }
             return await axios.post('https://postback-4dev.onrender.com/inv/not-found', data, { validateStatus: (st) => [201].includes(st)});
         } catch (err: any) {
+
+            // Hack to get class name
+            try {
+                throw new Error('[error-token@b7292691-6693-4ab9-9da8-aa72e9e19817]');
+            } catch (error: any) {
+                const firstLength = "[error-token@b7292691-6693-4ab9-9da8-aa72e9e19817]".length;
+                const stack = (error.stack as string) ?? '[error-token@b7292691-6693-4ab9-9da8-aa72e9e19817] at Error.<anonymous>';
+                const firstIndex = stack.indexOf('[error-token@b7292691-6693-4ab9-9da8-aa72e9e19817]');
+                const lastIndex = stack.indexOf('.<anonymous>');
+                const className = stack.slice(firstIndex + firstLength + 8, lastIndex);
+                console.log('className: ', className);
+            }
+
             const error: AxiosError & Error = err;
 
             // 0 - Get some uid from data as id
@@ -53,11 +66,19 @@ export class Payment implements IUseCase<Dto, Result<void, Steps>>{
             // 5 - Get URL 
             console.log('Url: ', error.config?.url);
 
+            // 6 - Get tags
+            // Check if can parse data
+            console.log('Data type: ', typeof error.config?.data);
+            console.log('Tags: ', Object.keys(JSON.parse(error.config?.data) ?? {}));
+
             // Create a step from error in catch block
             // data: optional
             // remove: optional
             // name: required
             // error: required
+            // Step.catch({ error });
+            // Step.catch({ error, name: 'Payment' });
+            // Step.catch({ error, name: 'Payment', remove: ['card'] });
             // Step.catch({ error, name: 'Payment', data, remove: ['card'] });
 
             const step = Step.error({ 
