1a58d7bb8c1b0abc516e3eb9e6b6993e02a231f2
4lessandrodev <alessandroadm@live.com>
2023-02-04 00:50:31 -0300

    feat: step as middleware

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import { bindLog, stackLog } from 'ts-logs';
+import express, { Response } from 'express';
+import { bindLog, Log, stackLog, Step } from 'ts-logs';
 import FindProduct from './use-case/find-product.use-case';
 import Login from './use-case/login.use-case';
 import MainUseCase from './use-case/main.use-case';
@@ -16,39 +16,87 @@ app.use(express.json());
 // bind Global log to request
 app.use(bindLog());
 
-app.post('/domain', async (req, res) => {
-    const log = req.log;
+app.post('/domain', async (req, res): Promise<Response> => {
+    const log = req.log as Log;
     const result = await main.execute(req.body);
-    if(result.isFail()){
+    if (result.isFail()) {
         const err = log.addStep(result.error());
         err.print(); // show error on terminal
         err.writeLocal(); // write local on /logs
         return res.status(400).json(err);
     }
     return res.status(200).json({ ok: true });
-})
+});
 
-app.post('/', (req, res) => {
+// chain middleware applying steps for each one
+app.post('/chain', (req, res, next): void => {
+
+    // STEP 1
+    const tags = ["chain", "log", "middleware"];
+    const step = Step.info({ name: 'Chain Step 1', message: 'Passed for step 1', tags });
+    req.log = req.log?.addStep(step);
+    next();
+
+}, (req, res, next): void => {
+
+     // STEP 2
+    const err = new Error("oops timeout. Error!");
+    const step = Step.catch(err);
+    req.log = req.log?.addStep(step);
+    next();
+
+}, (req, res, next): void => {
+
+     // STEP 3
+    const step = Step.error({ name: 'Chain Step 3', message: 'Something fails on step 2', statusCode: 400 });
+    req.log = req.log?.addStep(step);
+    next();
+
+}, (req, res, next): void => {
+
+     // STEP 4
+    const step = Step.stack({ name: 'Chain Step 4', message: 'Error in step 4', stack: 'Something went wrong!', statusCode: 500 });
+    req.log = req.log?.addStep(step);
+    next();
+
+}, (req, res, next): Response  => {
+
+     // STEP 5
+    const step = Step.warn({ message: 'Remember check some', name: 'Important' });
+    req.log = req.log?.addStep(step)
+    req.log?.writeLocal();
+    return res.status(200).json(req.log);
+
+});
+
+
+app.post('/', (req, res): void => {
     throw new Error('Forbidden Resource');
 });
 
-app.get('/ok', (req, res) => {
+app.get('/ok', (req, res): void => {
     res.status(200).json(req.log);
 });
 
-app.get('/success', (req, res) => {
+app.get('/success', (req, res): void => {
     res.status(200).json(req.log);
 });
 
-app.get('/fail', (req, res) => {
+app.get('/fail', (req, res): never => {
     throw new Error('Internal Server Error');
 });
 
-app.post('/error', (req, res) => {
+app.post('/error', (req, res): never => {
     throw new Error('Internal Server Error');
 });
 
 // intercept app error
-app.use(stackLog({ print: true, writeLocal: true, encrypt: false, sendAsResponse: true }));
+app.use(stackLog({
+    print: true,
+    writeLocal: true,
+    encrypt: false,
+    sendAsResponse: true,
+    remove: ["password", "card", "token"]
+}));
 
-app.listen(3000, () => console.log('running on http://localhost:3000'));
+app.listen(3000, (): void => console.log('running on http://localhost:3000'));
--- use-case/payment.use-case.ts
+++ use-case/payment.use-case.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { Fail, IUseCase, Ok, Result } from "rich-domain";
 import { Step, Steps } from "ts-logs";
 import { Dto as Product } from './find-product.use-case';
@@ -13,82 +13,16 @@ export class Payment implements IUseCase<Dto, Result<void, Steps>>{
     async execute(data: Dto): Promise<Result<void, Steps>> {
         try {
 
-            const isOk = ((Math.random() * 100) > 30);
+            const isOk = ((Math.random() * 100) > 70);
 
             if(isOk){
-                await axios.post(`https://postback-4dev.onrender.com/${data.product.id}`, data, { validateStatus: (st) => [201].includes(st)});
+                await axios.post(`https://postback-4dev.onrender.com/${data.product.id}`, data.user, { validateStatus: (st) => [201].includes(st)});
                 return Ok(null);
             }
-            return await axios.post('https://postback-4dev.onrender.com/inv/not-found', data, { validateStatus: (st) => [201].includes(st)});
-        } catch (err: any) {
-
-            // Hack to get class name
-            try {
-                throw new Error('[error-token@b7292691-6693-4ab9-9da8-aa72e9e19817]');
-            } catch (error: any) {
-                const firstLength = "[error-token@b7292691-6693-4ab9-9da8-aa72e9e19817]".length;
-                const stack = (error.stack as string) ?? '[error-token@b7292691-6693-4ab9-9da8-aa72e9e19817] at Error.<anonymous>';
-                const firstIndex = stack.indexOf('[error-token@b7292691-6693-4ab9-9da8-aa72e9e19817]');
-                const lastIndex = stack.indexOf('.<anonymous>');
-                const className = stack.slice(firstIndex + firstLength + 8, lastIndex);
-                console.log('className: ', className);
-            }
-
-            const error: AxiosError & Error = err;
-
-            // 0 - Get some uid from data as id
-            console.log('Uid: ', error.config?.data?.['id']);        
-
-            // 0 - Get Stack
-            console.log('Stack: ', error.stack);
-
-            // Identify if is AxiosError
-            console.log('IsAxios: ', error.stack?.includes('AxiosError'));
-
-            // 1- Get Error Message
-            console.log('Message: ', error.message);
+            return await axios.post('https://postback-4dev.onrender.com/inv/not-found', data.user, { validateStatus: (st) => [201].includes(st)});
+        } catch (err) {
             
-            // 2 - Geta try get data from response or request
-            // Remove keys from body option: ["card", "password"]
-            // Encrypt data? true | false
-            console.log('Request Data: ', error.config?.data);
-            console.log(typeof error.response?.data); // html é do tipo string | data tipo object
-            console.log('Response Data: ', error.response?.data);
-
-            // 3 - Get status code from response
-            const sts = error.response?.status;
-            const status = typeof sts === 'number' ? sts: 500;
-            console.log('Status: ', status);
-
-            // 4 - Get method
-            console.log('Method: ', (error.config?.method as string ?? 'NONE').toUpperCase());
-
-            // 5 - Get URL 
-            console.log('Url: ', error.config?.url);
-
-            // 6 - Get tags
-            // Check if can parse data
-            console.log('Data type: ', typeof error.config?.data);
-            console.log('Tags: ', Object.keys(JSON.parse(error.config?.data) ?? {}));
-
-            // Create a step from error in catch block
-            // data: optional
-            // remove: optional
-            // name: required
-            // error: required
-            // Step.catch({ error });
-            // Step.catch({ error, name: 'Payment' });
-            // Step.catch({ error, name: 'Payment', remove: ['card'] });
-            // Step.catch({ error, name: 'Payment', data, remove: ['card'] });
-
-            const step = Step.error({ 
-                message: (error as Error).message, 
-                name: 'Payment', 
-                stack: (error as Error).stack, 
-                method: 'POST',
-                statusCode: 500,
-                data: JSON.stringify(data)
-            })
+            const step = Step.catch(err as Error);
             return Fail(step);
         }
     }
